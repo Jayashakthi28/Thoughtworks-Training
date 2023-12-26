ce08db7a96f7e832df6479a58d40fc5dab4d7b88
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-12-26 08:49:41 +0530

    add tictactoe tdd

--- tic-tac-toe-tdd/cypress/e2e/spec.cy.js
+++ tic-tac-toe-tdd/cypress/e2e/spec.cy.js
@@ -37,6 +37,24 @@ describe("Check Winning condition for O", () => {
       .should("have.text", "Game Over : Winner is O");
   });
 });
+describe("Check Game Tie Condition", () => {
+  it("passes", () => {
+    const website = cy.visit("http://localhost:5173");
+    website.get("[data-box=00]").click(); //X
+    website.get("[data-box=02]").click(); //O
+    website.get("[data-box=01]").click(); //X
+    website.get("[data-box=10]").click(); //O
+    website.get("[data-box=12]").click(); //X
+    website.get("[data-box=11]").click(); //O
+    website.get("[data-box=20]").click(); //X
+    website.get("[data-box=22]").click(); //O
+    website.get("[data-box=21]").click(); //X
+
+    website
+      .get("[data-cy=result]")
+      .should("have.text", "Game Over : Tied");
+  });
+});
 describe("Check Double click on Same box", () => {
   it("passes", () => {
     const website = cy.visit("http://localhost:5173");
